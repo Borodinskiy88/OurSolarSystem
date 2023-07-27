d2a5c3e7cd25c7c727303bbdaf9354d23fbc80a1
Borodinskiy88 <a-borodinskiy@mail.ru>
2023-07-27 15:39:00 +0300

    logo

--- app/src/main/java/ru/borodinskiy/aleksei/oursolarsystem/ui/earth/EarthFragment.kt
+++ app/src/main/java/ru/borodinskiy/aleksei/oursolarsystem/ui/earth/EarthFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import ru.borodinskiy.aleksei.oursolarsystem.databinding.FragmentEarthBinding
@@ -13,8 +12,6 @@ class EarthFragment : Fragment() {
 
     private var _binding: FragmentEarthBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -23,14 +20,13 @@ class EarthFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val earthViewModel =
-            ViewModelProvider(this).get(EarthViewModel::class.java)
+            ViewModelProvider(this)[EarthViewModel::class.java]
 
         _binding = FragmentEarthBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textEarth
         earthViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+            binding.textEarth.text = it
         }
         return root
     }
