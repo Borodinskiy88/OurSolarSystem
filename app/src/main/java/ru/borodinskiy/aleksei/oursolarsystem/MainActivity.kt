d2d6d7eb3b22736f89d588ff22510a38c7a529fc
Borodinskiy88 <a-borodinskiy@mail.ru>
2023-07-27 19:29:39 +0300

    card_planets

--- app/src/main/java/ru/borodinskiy/aleksei/oursolarsystem/MainActivity.kt
+++ app/src/main/java/ru/borodinskiy/aleksei/oursolarsystem/MainActivity.kt
@@ -3,13 +3,11 @@ package ru.borodinskiy.aleksei.oursolarsystem
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.navigation.NavigationView
 import ru.borodinskiy.aleksei.oursolarsystem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -25,8 +23,8 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
+        val drawerLayout = binding.drawerLayout
+        val navView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
